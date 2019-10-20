9d9e237a909dc6cc7ec16ec12addfa4562982949
ziemniakoss <rawa.przemek@gmail.com>
2019-10-20 04:29:43 +0200

    Zapisywanie danych o rejstracjach do bazy Redis

---
+++ server/src/main/java/com/ziemniak/web/redis/RedisAccess.java
@@ -0,0 +1,63 @@
+package com.ziemniak.web.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import redis.clients.jedis.Jedis;
+
+import java.util.Map;
+
+
+//TODO replace with CrudeRepository
+public abstract class RedisAccess {
+    private static final String PREFIX = "user:";
+
+    public static boolean exists(String nick) {
+        Jedis j = new Jedis();
+        boolean result = j.exists(PREFIX + nick);
+        j.close();
+        return result;
+    }
+
+    public static void save(User user) {
+        if (user == null) {
+            throw new NullPointerException();
+        }
+        Jedis j = new Jedis();
+        String key = PREFIX + user.getNick();
+        j.hset(key, "password", user.getPassword());
+        j.close();
+    }
+
+    /**
+     * Fetches user data from db
+     *
+     * @param nick nick of user
+     * @return User from db or null if either nick was null or there was no user
+     * with given nick
+     */
+    public static User getUser(String nick) {
+        if (nick == null)
+            return null;
+        Jedis j = new Jedis();
+        Map<String, String> stringStringMap = j.hgetAll(PREFIX + nick);
+        j.close();
+        if (stringStringMap.isEmpty()) {
+            return null;
+        }
+        User user = new User();
+        user.setNick(nick);
+        user.setPassword(stringStringMap.get("password"));
+        return user;
+    }
+
+    public void delete(User user){
+        if(user == null){
+            return;
+        }
+        Jedis j = new Jedis();
+        j.del(user.getNick());
+        j.close();
+    }
+
+
+}
