1e06d1796a6484df9ae14d7973a58b5a0859bc66
ziemniakoss <rawa.przemek@gmail.com>
2019-11-17 14:50:21 +0100

    Równoległe budowanie i kontener redis

--- start.sh
+++ start.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-# budowanie
-cd client
-./mvnw install
-cd ../server
-./mvnw install
-cd ..
+#budowanie
+FAIL=0
+./build.sh seraver &
+./build.sh client &
+for job in `jobs -p`
+do
+echo $job
+    wait $job || let "FAIL+=1"
+done
+
+echo $FAIL
+
+if [ "$FAIL" == "0" ];
+then
+  echo "YAY!"
+else
+  echo "Nie udało sie skompilować! "
+  exit
+fi
+
 ( cd docker ; docker-compose up --build)
