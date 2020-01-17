1e81b1577619f5e2715f7d045699dc5c4f629994
ZIemniakoss <rawa.przemek@gmail.com>
2020-01-17 01:52:43 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- server/src/main/java/com/ziemniak/webserv/dto/RegisterRequestDTO.java
+++ server/src/main/java/com/ziemniak/webserv/dto/RegisterRequestDTO.java
@@ -5,23 +5,24 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @ApiModel(description = "Model containg request to create user with given username and password is database", value = "RegisterRequest")
-@EqualFields(first = "password", second = "validatePassword", message = "Passwords don't match")
+@EqualFields(first = "password", second = "validatePassword", message = "Hasła się nie zgadzają")
 public class RegisterRequestDTO {
-	@NotBlank
-	@Size(min = 4, max = 30, message = "Username must be between 4 and 30  character long")
-	@ApiModelProperty(notes = "Name for new user")
+	@NotBlank(message = "Nazwa użytkownika nie może być pusta")
+	@Size(min = 6, max = 30, message = "Nazwa użytkownika musi mieć od 6 do 30 znaków")//todo czy na pewno
 	private String username;
 
-	@ApiModelProperty(notes = "Password for new user. Must be at least 7 character long")
-	@NotBlank(message = "Password is required")
-	@Size(min = 7, message = "Password must be at least 7 characters long")
+	@NotBlank(message = "Proszę podać hasło")
+	@Size(min = 6, message = "Hasło musi mieć co najmniej 6 znaków")
+	@Pattern(regexp = ".*\\p{javaUpperCase}.*", message = "Hasło musi zawierać przynajmniej jedną wielką literę")
+	@Pattern(regexp = ".*\\p{javaDigit}.*", message = "Hasło musi zawierać przynajmniej jedną cyfrę")
+	@Pattern(regexp = ".*\\p{javaLowerCase}.*", message = "Hasło musi zawierać przynajmniej jedną małą literę")
 	private String password;
 
-	@ApiModelProperty(notes = "Validation of password. It must have same value as password")
-	@NotBlank(message = "Password validation required")
+	@NotBlank(message = "Proszę potwierdzić hasło")
 	private String validatePassword;
 
 	public String getUsername() {
